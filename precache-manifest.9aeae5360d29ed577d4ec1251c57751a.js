9a89f46beeb6f6ef8e597daa8f4b9309fa2b5cc6
dsolutions30 <65696006+dsolutions30@users.noreply.github.com>
2020-05-21 05:07:05 +0100

    Updates

---
+++ precache-manifest.9aeae5360d29ed577d4ec1251c57751a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6f6f03531c629e4aac7f0512450287",
+    "url": "/iPortfolio/index.html"
+  },
+  {
+    "revision": "f8ec11c5d2e61a222747",
+    "url": "/iPortfolio/static/css/main.a52f5468.chunk.css"
+  },
+  {
+    "revision": "519feb113464b272d05c",
+    "url": "/iPortfolio/static/js/2.14480b90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iPortfolio/static/js/2.14480b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ec11c5d2e61a222747",
+    "url": "/iPortfolio/static/js/main.290ea6a1.chunk.js"
+  },
+  {
+    "revision": "703a00305042cadc089b",
+    "url": "/iPortfolio/static/js/runtime-main.3a9d48ae.js"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/iPortfolio/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/iPortfolio/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/iPortfolio/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/iPortfolio/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/iPortfolio/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "7c8165a6f1990a40a773f5721c731b87",
+    "url": "/iPortfolio/static/media/hero-bg.7c8165a6.jpg"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/iPortfolio/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/iPortfolio/static/media/icofont.f6ab04ae.woff"
+  }
+]);
